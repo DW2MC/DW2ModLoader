4e26188f7f3e6c464e47efbaa4b690a8780f2b4a
froce <fwesen2@gmail.com>
2022-04-09 00:26:39 +0200

    Move read-only patch from ObjectDatabase to FileOdbBackend

---
+++ DW2Net6Win/PatchFileOdbBackend.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[PublicAPI]
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+[SuppressMessage("ReSharper", "RedundantAssignment")]
+[HarmonyPatch(typeof(Xenko.Core.Storage.FileOdbBackend), MethodType.Constructor, new[] { typeof(string), typeof(string), typeof(bool) })]
+public static class PatchFileOdbBackend
+{
+    public static bool Prefix(string vfsRootUrl, string indexName, ref bool isReadOnly)
+    {
+        if (!isReadOnly)
+        {
+            var writeablePaths = new[] { "/local", "/cache", "/tmp", "/roaming" };
+
+            foreach (var path in writeablePaths)                
+            {
+                if (vfsRootUrl.StartsWith(path))
+                    return true;
+            }
+            isReadOnly = true;
+        }
+        return true;
+    }
+}
