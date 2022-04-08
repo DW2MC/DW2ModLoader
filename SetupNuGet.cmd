9922b2a59ff6ff42d84eb63595f1faacdb4a1135
Tyler Young <tyler.young@impromptu.ninja>
2022-04-08 19:31:28 -0400

    fix source name in SetupNuGet.cmd

--- SetupNuGet.cmd
+++ SetupNuGet.cmd
@@ -2,4 +2,5 @@
 setlocal
 set /p U=GitHub User:
 set /p P=GitHub Token:
-dotnet nuget add source -n Tyler-IN -u "%U%" -p "%P%" --store-password-in-clear-text --configfile "%AppData%\NuGet\NuGet.Config"
+dotnet nuget add source -n GitHub-Tyler-IN -u "%U%" -p "%P%" --store-password-in-clear-text --configfile "%AppData%\NuGet\NuGet.Config"
+rem dotnet nuget add source -n GitHub-DW2MC -u "%U%" -p "%P%" --store-password-in-clear-text --configfile "%AppData%\NuGet\NuGet.Config"
