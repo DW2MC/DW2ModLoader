2e7e6cc75f096cc754f205d4b1b01526ab132cbb
froce <fwesen2@gmail.com>
2022-05-16 18:21:52 +0200

    Always allow custom races when using ModLoader

---
+++ DistantWorlds2.ModLoader.Patches/PatchStartNewGameDialog.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using DistantWorlds.Types;
+using DistantWorlds.UI;
+using HarmonyLib;
+using JetBrains.Annotations;
+
+namespace DistantWorlds2.ModLoader;
+
+[PublicAPI]
+[HarmonyPatch(typeof(StartNewGameDialog), nameof(StartNewGameDialog.DisableUnavailableRaces))]
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+public class EnableCustomRacesPatch
+{
+    [HarmonyPrefix]
+    public static bool Prefix()
+    {
+        return false;
+    }
+}
+
+[HarmonyPatch(typeof(StartNewGameDialog), nameof(StartNewGameDialog.RaceChanged))]
+public class HandleNonExistantRaceCrash
+{
+    [HarmonyPrefix]
+    public static bool Prefix(StartNewGameDialog __instance, ref Race ____SelectedRace, ScrollablePanel ___RaceSummary, ToggleButtonGroup ___Race, object sender, DWEventArgs args)
+    {
+        if (___Race.ToggledButtonIndex > 0)
+            return true;
+
+        __instance.YourEmpireGovernment_IndexSelected(-1);
+        ____SelectedRace = null;
+        ___RaceSummary.Clear();
+
+        return false;
+    }
+}
